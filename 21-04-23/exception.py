a0b1087686ef46cdba8a42deb16e6a2f5a8913d5
sudhanshu kumar <sudhanshukumar8816@gmail.com>
2023-04-21 13:02:13 +0530

    class codes

---
+++ 21-04-23/exception.py
@@ -0,0 +1,92 @@
+
+
+# print(1/0)
+
+
+#arr = [1,2,3]
+#print(arr[9])
+
+
+
+
+try:
+    x = 23/10
+except:
+    print("excetion occured")
+
+
+
+try:
+    x = 1/2
+except ZeroDivisionError:
+    print("zero divisio error occured")
+
+
+
+
+try:
+    x = 1/52
+except EOFError:
+    print("end of file occured")
+except ImportError:
+    print("import error exception occured")
+except RuntimeError:
+    print("zero didision error occured")
+else:
+    x = 20
+    #print("control is inside else")
+    #else will be execeuted after try when there is no exception occured
+
+
+#This is a dummy comment
+
+
+try:
+    x = 1/8
+except ZeroDivisionError as err:
+    print(err)
+
+
+#this is a dummy comment
+
+
+
+try:
+    x = 1/80
+except ZeroDivisionError as err:
+    print(err)
+finally:
+    print("finally will always gets executed")
+    #finnalyy block will always gets executed
+
+
+#this is a dummy coment
+
+
+
+arr = [1,2,3,4,5]
+
+try:
+    if len(arr) >=4:
+        raise ValueError("array length is greater or equal to 4")
+    else:
+        print("everything is fine")
+except ValueError as err:
+    print(err)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#this is a dummy comment
