5e1d7273cb58de8a2d001f8f7d1274024b66ed06
sudhanshu kumar <sudhanshukumar8816@gmail.com>
2023-04-24 12:05:11 +0530

    class codes

--- 24-04-23/index.py
+++ 24-04-23/index.py
@@ -35,4 +35,84 @@ def returningMultipleValues():
     return 10,20,30
 
 output = returningMultipleValues()
-print(output)
+#print(output)
+
+
+
+
+def printFibonaciSequence(n):
+    if n<=0:
+        print("invalid argument")
+        return -1
+    if n==1:
+        print(0)
+        return -1
+    if n==2:
+        print(0," ",1)
+        return -1
+    a = 0
+    b = 1
+    print(a," ",b)
+    count = 3
+    while count <=n:
+        next = a+b
+        print(next , end = " ")
+        a = b
+        b= next
+        count+=1
+#printFibonaciSequence(10)
+
+
+def findLcm(a,b):
+    if a>b:
+        greater = a
+    else:
+        greater = b
+
+    while True:
+        if((greater % a == 0) and (greater%b) == 0):
+            lcm = greater
+            break
+        greater+=1
+    return lcm
+
+print(findLcm(18,20))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#This is a dummy comment
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#This is a dummy comment
