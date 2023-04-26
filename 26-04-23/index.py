f6c55a3cf93ddd0f16f05b97e8587b13233e9647
sudhanshu kumar <sudhanshukumar8816@gmail.com>
2023-04-26 11:20:17 +0530

    class codes

--- 26-04-23/index.py
+++ 26-04-23/index.py
@@ -41,24 +41,31 @@ class SubSchool(School):
 #print(my_school.getAge())
 
 class Parent1:
-    def getParentName():
+    def getName(self):
         return "parent1"
-
 class Parent2:
-    def defParentName():
+    def defName():
         return "Parent2"
 class Parent3:
-    def getParentName():
+    def getName():
         return "Parent 3"
 
 class Child(Parent1 , Parent2 , Parent3):
-    def getAllParent():
-        #write custom logic here to get all parents name
+    def __init__(self):
+        self.getAllParent()
+    def getAllParent(self):
+        print("trying to get all parents of this class")
+        parent_list = []
+        for base in Child.__bases__:
+            parent_list.append(base)
+        print(parent_list)
+my_child = Child()
+#parent_list = []
+#print(my_child.__bases__)
+#for base in Child.__bases__:
+    #print(base,end=" ")
 
-
-
-
-
+#print(my_child.getName())
 
 
 
