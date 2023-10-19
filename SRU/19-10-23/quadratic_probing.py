f0b75b908a8a73b87a0aaea35d77c9565a971875
sudhanshu kumar <sudhanshukumar8816@gmail.com>
2023-10-19 09:56:45 +0530

    quadratic probing impl

---
+++ SRU/19-10-23/quadratic_probing.py
@@ -0,0 +1,45 @@
+
+
+
+
+
+def hash_fn(value):
+    return value%10
+
+def probe(hash_table,ci):
+    probe_number = 1
+    while hash_table[ci] != -1:
+        ci = ci + probe_number**2
+        if ci >= len(hash_table):
+            ci = ci%10
+        if hash_table[ci] == -1:
+            return ci
+        else:
+            probe_number+=1
+
+
+
+
+
+
+
+def quadratic_probing(search_keys,n):
+    hash_table = [-1 for i in range(n)]
+    for key in search_keys:
+        ci = hash_fn(key)
+        if hash_table[ci] == -1:
+            hash_table[ci] = key
+        else:
+            ni = probe(hash_table,ci)
+            hash_table[ni] = key
+    return hash_table
+
+
+def main():
+    search_keys = [34,54,65,78,32,98]
+    n = 10
+    hash_table = quadratic_probing(search_keys,n)
+    print(hash_table)
+
+
+main()
