ef15e90f8311302b0c12b692052ecb1896986d2b
sudhanshu kumar <sudhanshukumar8818@gmail.com>
2024-03-01 14:00:50 +0530

    more algos

--- pyramid-3-2/01-03-2024/Graph.py
+++ pyramid-3-2/01-03-2024/Graph.py
@@ -1,14 +1,20 @@
 
+minWeight, maxWeight = float('+inf'), float('-inf')
+
+longestPath, shortestPath = "", ""
+
+
 def buildGraph(n,edges):
     adjacencyMatrix = [[0 for i in range(n)] for j in range(n)]
     #matrix = [0 for i in range(n)]*n
     for edge in  edges:
         src = edge[0]
         des = edge[1]
-        adjacencyMatrix[src][des] = 1
-        adjacencyMatrix[des][src] = 1
+        wt = edge[2]
+        adjacencyMatrix[src][des] = wt
+        adjacencyMatrix[des][src] = wt
     return adjacencyMatrix
-
+  
 def printGraph(graph):
     for row in graph:
         for value in row:
@@ -20,7 +26,7 @@ def isPathExist(src,des,graph,visited):
         return True
     info = graph[src]
     for i in range(len(info)):
-        if info[i] == 1:
+        if info[i] != 0:
             if i not in visited:
                 visited.append(i)
                 flag = isPathExist(i,des,graph,visited)
@@ -28,21 +34,28 @@ def isPathExist(src,des,graph,visited):
                     return True
     return False
 
-def printAllPaths(src,des,graph,visited,psf):
+def printAllPaths(src,des,graph,visited,psf,wsf):
+    global minWeight, maxWeight, longestPath, shortestPath
     if src == des:
-        print(psf)
+        print(psf,"psfff")
+        if len(psf)>len(longestPath) or len(longestPath) == 0:
+            longestPath = psf
+        if len(shortestPath) == 0 or len(psf) < len(shortestPath):
+            shortestPath = psf
+        if wsf > maxWeight: maxWeight = wsf
+        if wsf < minWeight : minWeight = wsf
     info = graph[src]
     for i in range(len(info)):
-        if info[i] == 1:
+        if info[i] != 0:
             if i not in visited:
                 visited.append(i)
-                printAllPaths(i,des,graph,visited,psf+str(i))
+                printAllPaths(i,des,graph,visited,psf+str(i),wsf+info[i])
                 visited.remove(i)
                 # if flag == True:
                 #     return True
     
-edges = [[0,2],[0,1],[1,3],[4,5],[5,6],
-            [6,7],[7,4],[3,2],[3,4]]
+edges = [[0,2,8],[0,1,-9],[1,3,7],[4,5,56],[5,6,1],
+            [6,7,69],[7,4,67],[3,2,65],[3,4,8]]
 
 #nbr = [[1,2],[3],[],[4,2],[5],[6],[7],[4]]
 
@@ -51,9 +64,13 @@ graph = buildGraph(nodes,edges)
 #printGraph(graph)
 visited = []
 src = 0
-des = 6
+des = 2
 visited.append(src)
 #print(isPathExist(src,des,graph,visited))
-printAllPaths(src,des,graph,visited,str(src))
+printAllPaths(src,des,graph,visited,str(src),0)
 
+print(longestPath,"longest")
+print(shortestPath,"shortest")
+print(minWeight,"min-weight")
+print(maxWeight,"max-weight")
 
