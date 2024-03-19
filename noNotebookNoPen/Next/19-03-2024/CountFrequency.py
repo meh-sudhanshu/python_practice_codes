3c6fd51e7a2ebe7519dd5a947a6d25272f8e21de
sudhanshu kumar <sudhanshukumar8818@gmail.com>
2024-03-19 16:44:52 +0530

    More codes

---
+++ noNotebookNoPen/Next/19-03-2024/CountFrequency.py
@@ -0,0 +1,7 @@
+# 
+
+my_dict = {"a":10,"b":20,"c":30}
+for k in my_dict:
+    print(k,my_dict.get(k))
+# for item in my_dict.list():
+#     print(item)
---
+++ noNotebookNoPen/Next/19-03-2024/DataStructure.py
@@ -0,0 +1,6 @@
+# 
+
+my_dict = {"name":"sudhanshu","a":10,"b":20}
+print(type(my_dict))
+
+arr = [1,4,2,3,2,1,3,2,3,2,4,2,,4,3,2,1]
