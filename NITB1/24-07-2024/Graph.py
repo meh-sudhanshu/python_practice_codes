add4d678328158fdc80e6ae98f5a9d2961fd4379
sudhanshu kumar <sudhanshukuar8816@gmail.com>
2024-07-24 11:20:45 +0530

    more codes

--- NITB1/24-07-2024/Graph.py
+++ NITB1/24-07-2024/Graph.py
@@ -17,13 +17,32 @@ def printGraph(graph):
         print()
 
 
+def hasPath(graph,src,des,visited):
+    if src == des:
+        return True
+
+    nbrs = graph[src]
+    for i in range(len(nbrs)):
+        if nbrs[i] == 1:
+            if visited[i] == 0:
+                visited[i] = 1
+                ans = hasPath(graph,i,des,visited)
+                if ans == True:
+                    return True
+    return False
+
 
 
 
 def main():
-    edges = [[0,1],[0,2],[1,3],[2,3],[3,4],[4,5],[4,6],[5,7],[6,7]]
+    edges = [[0,1],[0,2],[1,3],[2,3],[4,5],[4,6],[5,7],[6,7]]
     n = 8
     graph = buildGraph(edges,n)
-    printGraph(graph)
+    #printGraph(graph)
+    src = 3
+    des = 5
+    visited = [0 for i in range(n)]
+    ans = hasPath(graph,src,des,visited)
+    print(ans)
 
 main()
