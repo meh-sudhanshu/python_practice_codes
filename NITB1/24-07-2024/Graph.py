e25d186e32c00a500b6715fed08d10f468eb4df3
sudhanshu kumar <sudhanshukuar8816@gmail.com>
2024-07-24 14:26:03 +0530

    more codes

--- NITB1/24-07-2024/Graph.py
+++ NITB1/24-07-2024/Graph.py
@@ -43,17 +43,35 @@ def printAllPath(graph,src,des,visited,psf):
                     str(src)+"->"+str(i) if psf == "" else psf+"->"+str(i)
                 )
                 visited[i] = 0
+
+def buildGraphWithAdjacencyList(edges,n):
+    graph = [[] for i in range(n)]
+    for edge in edges:
+        src = edge[0]
+        des = edge[1]
+        wt  = edge[2]
+        graph[src].append([src,des,wt])
+        graph[des].append([des,src,wt])
+    return graph
+
+def printGraphInAdjacencyList(graph):
+    for node in graph:
+        for edges in node:
+            print(edges,end=" ")
+
 def main():
-    edges = [[0,1],[0,2],[1,3],[2,3],[4,5],[3,4],[4,6],[5,7],[6,7]]
+    edges = [[0,1,10],[0,2,3],[1,3,-3],[2,3,23],[4,5,123],[3,4,23],[4,6,3],[5,7,9],[6,7,9]]
     n = 8
-    graph = buildGraph(edges,n)
-    #printGraph(graph)
-    src = 0
-    des = 7
-    visited = [0 for i in range(n)]
-    visited[src] = 1
-    #ans = hasPath(graph,src,des,visited)
-    #print(ans)
-    printAllPath(graph,src,des,visited,"")
+    # graph = buildGraph(edges,n)
+    # #printGraph(graph)
+    # src = 0
+    # des = 7
+    # visited = [0 for i in range(n)]
+    # visited[src] = 1
+    # #ans = hasPath(graph,src,des,visited)
+    # #print(ans)
+    # printAllPath(graph,src,des,visited,"")
+    graph = buildGraphWithAdjacencyList(edges,n)
+    printGraphInAdjacencyList(graph)
 
 main()
