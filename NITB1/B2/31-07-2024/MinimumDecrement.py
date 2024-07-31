8f6d7417f3be2fb31823892262c8625aabdff394
sudhanshu kumar <sudhanshukuar8816@gmail.com>
2024-07-31 11:29:39 +0530

    more codes

---
+++ NITB1/B2/31-07-2024/MinimumDecrement.py
@@ -0,0 +1,29 @@
+
+
+
+def getMinDecrement(arr):
+    allXor = 0
+    for e in arr: allXor = allXor ^ e
+
+    ans = -1
+
+    for e in arr:
+        allXorExceptE = allXor ^ e
+
+        if allXorExceptE <= e:
+            currAns = e - allXorExceptE
+            if ans == -1 or currAns < ans:
+                ans = currAns
+    
+    return ans
+
+
+
+
+
+def main():
+    arr = [7,3,8,9,5,12]
+    ans = getMinDecrement(arr)
+    print(ans)
+
+main()
