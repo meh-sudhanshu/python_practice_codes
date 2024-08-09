ef016559b28b107b14e017a47d82a1a1259a81e0
sudhanshu kumar <sudhanshukuar8816@gmail.com>
2024-08-09 15:38:13 +0530

    more codes

--- NITB1/B2/09-08-2024/Graph.py
+++ NITB1/B2/09-08-2024/Graph.py
@@ -1,9 +1,28 @@
 
 
+def hasPath(graph,src,des,visited):
+    if src == des:
+        return True
+    
+    nbrs = graph[src]
+
+    for i in range(len(nbrs)):
+        if nbrs[i] == 1:
+            if visited[i] == 0:
+                visited[i] = 1
+                ans = hasPath(graph,i,des,visited)
+                if ans == True:
+                    return True
+    return False
+
+
+
+
+
+
 
 def buildGraph(edges,n):
-    graph = [[0 for i in range(n)] for j in range(n)]
-    
+    graph = [[0 for i in range(n)] for j in range(n)] 
     for edge in edges:
         src = edge[0]
         des = edge[1]
@@ -30,11 +49,18 @@ def printGraph(graph):
 
 
 def main():
-    edges = [[0,1],[0,2],[1,3],[2,3],[3,4],[4,5],[4,6],[6,7],[5,7]]
+    edges = [[0,1],[0,2],[1,3],[2,3],[4,5],[4,6],[6,7],[5,7]]
     n = 8
     # bidirectional, non-weighted no self cycle graph
     graph = buildGraph(edges,n)
-    printGraph(graph)
+    #printGraph(graph)
+    src = 0
+    des = 2
+    visited = [0 for i in range(n)]
+    visited[src] = 1
+    
+    isPathExist = hasPath(graph,src,des,visited)
+    print(isPathExist)
 
 
 main()
